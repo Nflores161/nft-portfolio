ae5be66f349f3b0529f35f53da8e286fd8e45af5
Nick Flores-Tober <nflores161@gmail.com>
2021-10-04 21:33:45 -0400

    added layout for portfolio

--- src/Components/PortfolioCard.js
+++ src/Components/PortfolioCard.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const PortfolioCard = () => {
   return (
     <Container>
-      <Row lg={3}>
-        <h1>Title</h1>
-        <img scr="#" alt="none"></img>
-        <p>description</p>
+      <Row>
+        <Col xs={6} md={4}>
+          <h1>Title</h1>
+          <img scr="#" alt="none"></img>
+          <p>description</p>
+        </Col>
+        <Col xs={6} md={4}>
+          <h1>Title</h1>
+          <img scr="#" alt="none"></img>
+          <p>description</p>
+        </Col>
+        <Col xs={6} md={4}>
+          <h1>Title</h1>
+          <img scr="#" alt="none"></img>
+          <p>description</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6} md={4}>
+          <h1>Title</h1>
+          <img scr="#" alt="none"></img>
+          <p>description</p>
+        </Col>
+        <Col xs={6} md={4}>
+          <h1>Title</h1>
+          <img scr="#" alt="none"></img>
+          <p>description</p>
+        </Col>
+        <Col xs={6} md={4}>
+          <h1>Title</h1>
+          <img scr="#" alt="none"></img>
+          <p>description</p>
+        </Col>
       </Row>
     </Container>
   )
